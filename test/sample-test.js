99e0c1e75859aa53d169c2137a2310ecd7f52eb0
hemanth bhushan <hemanthbhushan6@gmail.com>
2022-08-25 08:02:16 +0530

    commit message

--- test/sample-test.js
+++ test/sample-test.js
@@ -3,13 +3,13 @@ const { ethers, network } = require("hardhat");
 
 
 describe("PAXGImplementation contract",()=>{
-    let  paxg,paxgProxy,owner,signer1,signer2,proxy;
+    let  paxg,paxgProxy,ownerA,signer1,signer2,proxy,signer;
 
     beforeEach(async()=>{
-      [admin,owner,signer1,signer2] = await ethers.getSigners();
-      const Paxg = await ethers.getContractFactory("PAXGImplementation",owner.address);
+      [_,ownerA,admin,signer1,signer2] = await ethers.getSigners();
+      const Paxg = await ethers.getContractFactory("PAXGImplementation",ownerA.address);
       paxg = await Paxg.deploy();
-      const Proxy = await ethers.getContractFactory("AdminUpgradeabilityProxy");
+      const Proxy = await ethers.getContractFactory("AdminUpgradeabilityProxy",admin.address);
       proxy = await Proxy.deploy(paxg.address);
       paxgProxy =   Paxg.attach(proxy.address);
 
@@ -20,19 +20,22 @@ describe("testing PAXG implementation",()=>{
       expect(paxgProxy.initialize()).to.be.revertedWith("already initialized");
    })
 it("checking symbol and name ",async ()=>{
-  const name = await  paxgProxy.name();
-    // expect(name).to.equal("Paxos Gold");
+  const name = await  paxgProxy.connect(signer1).name();
+   
   
-  const symbol = await  paxgProxy.symbol();
-  const owner1 = await paxgProxy.owner();
-  expect(await paxgProxy.supplyController()).to.equal(owner.address);
-  // expect(await paxgProxy.feeController()).to.equal(owner.address);
-  // expect(await paxgProxy.feeRecipient()).to.equal(owner.address);
-  // expect(owner1).to.equal(owner.address);
-  // expect(name).to.equal("Paxos Gold");
-  // expect(symbol).to.equal("PAXG");
+  const symbol = await  paxgProxy.connect(signer1).symbol();
+  //in proxy we need to initialize the function initialize cuz proxy the constructor doesnt work
+  await paxgProxy.connect(ownerA).initialize();
+  const owner1 = await paxgProxy.connect(signer1).owner();
+  expect(await paxgProxy.connect(signer1).supplyController()).to.equal(ownerA.address);
+  expect(await paxgProxy.connect(signer1).feeController()).to.equal(ownerA.address);
+  expect(await paxgProxy.feeRecipient()).to.equal(ownerA.address);
+  expect(owner1).to.equal(ownerA.address);
+  expect(name).to.equal("Paxos Gold");
+  expect(symbol).to.equal("PAXG");
 
 })
+it("check the ")
 
 
 })
